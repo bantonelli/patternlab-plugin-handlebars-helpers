f3f23a0658a9d1f2528458d9f0d9dc7e87239378
Brandon Antonelli <bantonelli07@gmail.com>
2017-04-29 20:52:01 -0400

    Modify registerHelper

--- src/register-helper.js
+++ src/register-helper.js
@@ -1,17 +1,25 @@
 'use strict';
 
+// function registerHelper(patternlab, Handlebars) {
+//   Handlebars.registerHelper('listitems', function(options) {
+//     let html = '';
+//     let listitems = patternlab.listitems[options.hash.size];
+//     listitems.forEach(function(listitem) {
+//       if (patternlab.config.debug) {
+//         console.log(listitem);
+//       }
+//       html = html + options.fn(listitem)
+//     })
+//     return html;
+//   });
+// }
+var helpers = require('handlebars-helpers');
+
 function registerHelper(patternlab, Handlebars) {
-  Handlebars.registerHelper('listitems', function(options) {
-    let html = '';
-    let listitems = patternlab.listitems[options.hash.size];
-    listitems.forEach(function(listitem) {
-      if (patternlab.config.debug) {
-        console.log(listitem);
-      }
-      html = html + options.fn(listitem)
-    })
-    return html;
+  helpers({
+    handlebars: Handlebars
   });
 }
+
 
 module.exports = registerHelper;
